ea42c0f760745feab1c64d2b8aa4b9175d0d62d0
Anudeep Muktinutalapati <m.anudeep2000@gmail.com>
2023-11-07 23:18:02 +0530

    Removed auto polling

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -209,7 +209,7 @@ export default function Users() {
   const { isLoading, isError, errorMessage, data } = useFetch(
     `${process.env.API_ENDPOINT}/watchstamps`,
     environmentOption,
-    true,
+    false,
     10000
   );
 
